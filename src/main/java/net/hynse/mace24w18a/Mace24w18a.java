33dff24fd066045c501f1c7e5573f2edc81a7ae7
MidnightTale <midnighttale24@gmail.com>
2024-05-26 06:30:08 +0700

    get nerf a little

---
+++ src/main/java/net/hynse/mace24w18a/Mace24w18a.java
@@ -0,0 +1,17 @@
+package net.hynse.mace24w18a;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mace24w18a extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
